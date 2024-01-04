3e31f025c92d89b8d88e3f176a80127e2867abf5
Karina <Karina@10.0.0.31>
2024-01-03 21:43:17 -0500

    fixed brackets/spaces

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -12,10 +12,11 @@ public class DemoA {
 
 	//Sprint 2 automation
 	public static void add (int a, int b) {
-		System.out.println(a+ b);
+		System.out.println(a + b);
 	}
 
         //Coming from GitHub
 	public static void subtract (int a, int b) {
 		System.out.println (a - b);
+}
 }
