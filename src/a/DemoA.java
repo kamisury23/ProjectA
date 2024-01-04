3b5b0993c84d81f1e987621814601900b1bbb939
Karina <Karina@10.0.0.31>
2024-01-03 21:50:58 -0500

    Implemented multiply ()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -1,27 +1,36 @@
 package a;
 
 public class DemoA {
-	public static void main (String args[]) {
-		
+	public static void main(String args[]) {
+
 		System.out.println("January");
-		
+
 		System.out.println("Sprint 1  work...");
 		System.out.println("Sprint 2  work...");
-		
+
 	}
 
-	//Sprint 2 automation
-	public static void add (int a, int b) {
+	// Sprint 2 automation
+	public static void add(int a, int b) {
 		System.out.println(a + b);
 	}
 
-        //Coming from GitHub
-	public static void subtract (int a, int b) {
-		System.out.println (a - b);
+	// Coming from GitHub
+	public static void subtract(int a, int b) {
+		System.out.println(a - b);
+
 	}
-	//Coming from GitHub
-	public static void divide (int a, int b) {
-		System.out.println (a / b);
-		
-}
+
+	// Coming from GitHub
+	public static void divide(int a, int b) {
+		System.out.println(a / b);
+
+	}
+
+	// Coming from Eclipse
+	public static void multiply(int a, int b) {
+		System.out.println(a * b);
+
+	}
+
 }
