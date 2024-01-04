82885cb4b35dc457055b329d3e4847862d14a50c
kamisury23 <155509146+kamisury23@users.noreply.github.com>
2024-01-03 21:40:17 -0500

    Update DemoA.java

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -14,5 +14,8 @@ public class DemoA {
 	public static void add (int a, int b) {
 		System.out.println(a+ b);
 	}
-	
+
+        //Coming from GitHub
+	public static void subtract (int a, int b) {
+		System.out.println (a - b);
 }
